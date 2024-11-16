68e49f2c16ebeaeaba5590bd7764e31ffb0da8eb
mitigd <mitigatingdiscord@proton.me>
2024-11-16 15:32:22 -0600

    added meson build w/msys2 support

--- examples/win32/main.cpp
+++ examples/win32/main.cpp
@@ -1,7 +1,27 @@
+#include <windows.h>
 #include <stdio.h>
 #include "..\..\include\distormx.h"
 
+typedef int(WINAPI* tMessageBoxA)(
+    HWND hWnd,
+    LPCSTR lpText,
+    LPCSTR lpCaption,
+    UINT uType
+);
+
+tMessageBoxA pMessageBoxA;
+
 unsigned int(*g_orig_f1)(unsigned int, unsigned int) = NULL;
+
+int WINAPI dMessageBoxA(
+    HWND hWnd, 
+    LPCSTR lpText, 
+    LPCSTR lpCaption, 
+    UINT uType) {
+
+    return pMessageBoxA(hWnd, "Hooked MessageBoxA", "Hooked", uType);
+
+}
 
 __declspec(noinline) unsigned int f1(unsigned int a, unsigned int b)
 {
@@ -18,10 +38,12 @@ __declspec(noinline) unsigned int f2(unsigned int a, unsigned int b)
 
 int main()
 {
+
+	/* Original Example */
 	f1(1, 1);
 
 	g_orig_f1 = f1;
-	if (!distormx_hook((void **)&g_orig_f1, f2)) {
+	if (!distormx_hook((void **)&g_orig_f1, reinterpret_cast<void*>(f2))) {
 		printf("failed hooking");
 		return 1;
 	}
@@ -29,6 +51,26 @@ int main()
 	distormx_unhook((void *)&g_orig_f1);
 	f1(3, 3);
 	distormx_destroy();
+
+	/* MessageBox Example */
+	HMODULE hModule = LoadLibraryA("user32.dll");
+    if (hModule != nullptr) {
+
+        // Hook for MessageBoxA
+        pMessageBoxA = (tMessageBoxA)GetProcAddress(hModule, "MessageBoxA");
+        if (pMessageBoxA != nullptr) {
+            distormx_hook(
+                &(PVOID&)pMessageBoxA,
+                reinterpret_cast<void*>(dMessageBoxA));
+        } else {
+            printf("Failed to get address of MessageBoxA!");
+        }
+        
+    } else {
+        printf("Failed to load user32.dll!");
+    }
+    
+    MessageBoxA(NULL, "Original MessageBoxA", "Original", MB_OK);
 
 	return 0;
 }
